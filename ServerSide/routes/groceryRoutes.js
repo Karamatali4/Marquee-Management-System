73d38f623f831357b07293513df506c1218aadbe
Karamatali4 <maqpoon4@gmail.com>
2025-07-04 16:28:14 +0500

    add update and delete routes in groceries

--- ServerSide/routes/groceryRoutes.js
+++ ServerSide/routes/groceryRoutes.js
@@ -3,14 +3,24 @@ const groceryRoutes = express.Router();
 const {
   authMiddleware,
   staffMiddleware,
+  adminMiddleware,
 } = require("../middleware/authMiddleware");
 const {
   createGroceryData,
   getGroceryData,
+  deleteGrocery,
+  updateGrocery,
 } = require("../controllers/groceryController");
 
 groceryRoutes.get("/", authMiddleware, staffMiddleware, getGroceryData);
 
 groceryRoutes.post("/", authMiddleware, staffMiddleware, createGroceryData);
+
+
+//  delete a grocerie
+groceryRoutes.delete("/groceries/:id", authMiddleware, adminMiddleware, deleteGrocery);
+
+// Optionally allow update
+groceryRoutes.put("/groceries/:id", authMiddleware, adminMiddleware, updateGrocery);
 
 module.exports = groceryRoutes;
