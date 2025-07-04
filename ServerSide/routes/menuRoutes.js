8641e574f250e9929f2f7c8720fa5eb5790f4997
Karamatali4 <maqpoon4@gmail.com>
2025-07-04 17:34:38 +0500

    create routes such as update and delete

--- ServerSide/routes/menuRoutes.js
+++ ServerSide/routes/menuRoutes.js
@@ -3,9 +3,21 @@ const menuRoutes = express.Router();
 const {
   authMiddleware,
   staffMiddleware,
+  adminMiddleware,
 } = require("../middleware/authMiddleware");
-const createMenuData = require("../controllers/menuController");
 
-menuRoutes.post("/",authMiddleware,  createMenuData);  //staffMiddleware,
+const {createMenu,deleteMenu,updateMenu} = require("../controllers/menuController")
+
+menuRoutes.post("/",authMiddleware, createMenu);  //staffMiddleware,
+
+
+
+
+//  delete a menu
+menuRoutes.delete("/menu/:id", authMiddleware, adminMiddleware, deleteMenu);
+
+// Optionally allow update menu single data
+menuRoutes.put("/menu/:id", authMiddleware, adminMiddleware, updateMenu);
+
 
 module.exports = menuRoutes;
