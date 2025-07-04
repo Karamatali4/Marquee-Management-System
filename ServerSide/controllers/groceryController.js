e3616765546316a50a101208def692b07bca13ff
Karamatali4 <maqpoon4@gmail.com>
2025-07-04 16:12:54 +0500

    add functions for update and delete single data for groceries

--- ServerSide/controllers/groceryController.js
+++ ServerSide/controllers/groceryController.js
@@ -25,4 +25,43 @@ const createGroceryData = async (req, res) => {
   }
 };
 
-module.exports = { getGroceryData, createGroceryData };
+
+
+
+// DELETE grocery (Admin Only)
+const deleteGrocery = async (req, res) => {
+  try {
+    const grocery = await Grocery.findByIdAndDelete(req.params.id);
+    if (!grocery) {
+      return res.status(404).json({ error: "grocery not found" });
+    }
+    res.status(200).json({ msg: "grocery deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting grocery:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+// UPDATE grocery (Admin Only)
+const updateGrocery = async (req, res) => {
+  try {
+    const updatedGrocery = await Grocery.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedGrocery) {
+      return res.status(404).json({ error: "Grocery not found" });
+    }
+
+    res.status(200).json({
+      msg: "Grocery updated successfully",
+      Grocery: updatedGrocery,
+    });
+  } catch (error) {
+    console.error("Error updating Grocery:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+module.exports = { getGroceryData, createGroceryData,deleteGrocery,updateGrocery };
