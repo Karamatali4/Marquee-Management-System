06079a10fc945938e8d2b65798ec895ec90d6f33
Karamatali4 <maqpoon4@gmail.com>
2025-07-04 17:34:11 +0500

    create function such as update and delete operation

--- ServerSide/controllers/menuController.js
+++ ServerSide/controllers/menuController.js
@@ -1,7 +1,7 @@
 const Menu = require("../models/Menu");
 
 // create menu
-const createMenuData = async (req, res) => {
+const createMenu = async (req, res) => {
   try {
     const newMenu = new Menu(req.body);
     await newMenu.save();
@@ -12,4 +12,45 @@ const createMenuData = async (req, res) => {
   }
 };
 
-module.exports = createMenuData;
+
+
+
+// DELETE menu (Admin Only)
+const deleteMenu = async (req, res) => {
+  try {
+    const menu = await Menu.findByIdAndDelete(req.params.id);
+    if (!menu) {
+      return res.status(404).json({ error: "menu not found" });
+    }
+    res.status(200).json({ msg: "menu deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting menu:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+// UPDATE menu 
+const updateMenu = async (req, res) => {
+  try {
+    const updatedmenu = await   Menu.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedmenu) {
+      return res.status(404).json({ error: "menu not found" });
+    }
+
+    res.status(200).json({
+      msg: "menu updated successfully",
+      menu: updatedmenu,
+    });
+  } catch (error) {
+    console.error("Error updating Menu:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+
+module.exports = {createMenu,deleteMenu,updateMenu};
