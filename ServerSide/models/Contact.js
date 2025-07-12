f0307f64bf92d6b35915cb171a5c29e0c630621a
Karamatali4 <maqpoon4@gmail.com>
2025-07-12 17:11:35 +0500

    changes ContactSchema name

--- ServerSide/models/Contact.js
+++ ServerSide/models/Contact.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
-  contactName: String,
- contactEmail: String,
-  contactPhone: Number,
-  contactMessage: String,
+  name: String,
+ email: String,
+  phone: Number,
+  message: String,
 });
 
 module.exports = mongoose.model("Contact", contactSchema);
