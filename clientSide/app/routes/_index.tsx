5fc8af2b88b360e1373c83a9989f9f537437f710
Karamatali4 <maqpoon4@gmail.com>
2025-07-12 16:00:04 +0500

    changes bg color

--- clientSide/app/routes/_index.tsx
+++ clientSide/app/routes/_index.tsx
@@ -7,12 +7,12 @@ export default function Index() {
   return (
     <>
     <Header/>
-    <main className="flex items-center justify-center mt-10 mb-24">
+    <main className="flex items-center justify-center pt-10 pb-24 ">
       <div className="text-center">
         <h1 className="text-3xl font-bold mb-4"><img src="/logo.png" alt="" /></h1>
         
-        <Link to="/login" className="bg-amber-700 text-white px-4 py-2 rounded">Login</Link> 
-        <Link to="/register" className="bg-amber-700 text-white px-4 py-2 rounded ms-5">register</Link>
+        <Link to="/login" className="bg-amber-700 text-white px-4 py-2 rounded hover:bg-amber-600">Login</Link> 
+        <Link to="/register" className="bg-amber-700 text-white px-4 py-2 rounded ms-5 hover:bg-amber-600">register</Link>
       </div>
     </main>
     <Footer/>
