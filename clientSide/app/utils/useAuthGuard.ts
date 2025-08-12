f8ff854006032dc1bb5aa82242bf85b68ff32423
kimijan1990 <1980253@szabist-isb.pk>
2025-08-13 00:06:49 +0500

    add state variable loading when an unauthorized user can access auth page

--- clientSide/app/utils/useAuthGuard.ts
+++ clientSide/app/utils/useAuthGuard.ts
@@ -1,14 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "@remix-run/react";
 
 export function useAuthGuard(allowedRole: string) {
   const navigate = useNavigate();
+  const [isAuthorized, setIsAuthorized] = useState(false);
 
   useEffect(() => {
     const role = localStorage.getItem("role");
 
     if (!role || role !== allowedRole) {
       navigate("/login");
+    } else {
+      setIsAuthorized(true);
     }
   }, [navigate, allowedRole]);
+
+  return isAuthorized;
 }
