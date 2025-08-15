c19c9a31fce623354c866b0610ad368b4f7b1bde
kimijan1990 <1980253@szabist-isb.pk>
2025-08-15 18:38:34 +0500

    changes

--- clientSide/app/components/Sidebar.tsx
+++ clientSide/app/components/Sidebar.tsx
@@ -1,6 +1,10 @@
 import { Link } from "@remix-run/react";
 import { useState, useRef, useEffect } from "react";
 import { Menu, X } from "lucide-react";
+import { BiFoodMenu, BiLogOutCircle } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+import { CiHome } from "react-icons/ci";
+import { TbBrandBooking } from "react-icons/tb";
 
 export default function Sidebar({ role }: { role: string }) {
   const [isOpen, setIsOpen] = useState(true);
@@ -45,42 +49,46 @@ export default function Sidebar({ role }: { role: string }) {
         ref={sidebarRef}
         className={`${
           isOpen ? "translate-x-0" : "-translate-x-full "
-        } md:translate-x-0 fixed md:static sm:w-48 lg:w-56 bg-amber-600 text-amber-100 rounded-e-lg h-screen p-4 shadow-md transition-transform duration-300 ease-in-out z-40`}
+        } md:translate-x-0 fixed md:static sm:w-48 lg:w-56 bg-white text-amber-950 rounded-e-lg h-screen p-4 shadow-2xl  transition-transform duration-300 ease-in-out z-40 border-e-2`}
       >
-        <h2 className="text-xl font-bold mb-4 capitalize mt-10">{role} Menu</h2>
-        <ul className="space-y-3 text-amber-50 ">
-          <li>
-            <Link to={`/dashboard/${role}`} className="hover:text-amber-200">Home</Link>
+        <h2 className="text-xl font-bold mb-4 capitalize mt-10 ">{role.toUpperCase()} MENU</h2>
+        <ul className="space-y-3 text-amber-950">
+          <li className="flex justify-center items-center gap-3"> <span><CiHome className="text-amber-900 text-xl" />
+</span>
+            <Link to={`/dashboard/${role}`} className="hover:text-amber-200 text-lg">Home</Link>
           </li>
 
           {role === "admin" && (
             <>
-              <li><Link to="/admin/bookings" className="hover:text-amber-200">Bookings</Link></li>
-              <li><Link to="/admin/menu" className="hover:text-amber-200">Menu</Link></li>
-              <li><Link to="/admin/grocery" className="hover:text-amber-200">Grocery</Link></li>
-              <li><Link to="/admin/salary" className="hover:text-amber-200">Salary</Link></li>
-              <li><Link to="/admin/users" className="hover:text-amber-200">Users</Link></li>
+              <li className="flex justify-center items-center gap-3"><Link to="/admin/bookings" className="hover:text-amber-200">Bookings</Link></li>
+              <li className="flex justify-center items-center gap-3"><Link to="/admin/menu" className="hover:text-amber-200">Menu</Link></li>
+              <li className="flex justify-center items-center gap-3"><Link to="/admin/grocery" className="hover:text-amber-200">Grocery</Link></li>
+              <li  className="flex justify-center items-center gap-3"><Link to="/admin/salary" className="hover:text-amber-200">Salary</Link></li>
+              <li className="flex justify-center items-center gap-3"><Link to="/admin/users" className="hover:text-amber-200">Users</Link></li>
             </>
           )}
 
           {role === "staff" && (
             <>
-              <li><Link to="/staff/bookings" className="hover:text-amber-200">Bookings</Link></li>
-              <li><Link to="/staff/menu" className="hover:text-amber-200">Menu</Link></li>
-              <li><Link to="/staff/grocery" className="hover:text-amber-200">Grocery</Link></li>
-              <li><Link to="/staff/salary" className="hover:text-amber-200">Salary</Link></li>
+              <li className="flex justify-center items-center gap-1"> <span><TbBrandBooking className="text-amber-900 text-xl"/>
+</span><Link to="/staff/bookings" className="hover:text-amber-200 text-lg">Bookings</Link></li>
+              <li className="flex justify-center items-center gap-3"> <span><BiFoodMenu className="text-amber-900 text-xl" />
+</span><Link to="/staff/menu" className="hover:text-amber-200 text-lg">Menu</Link></li>
+
+              <li className="flex justify-center items-center gap-3"><Link to="/staff/grocery" className="hover:text-amber-200 text-lg">Grocery</Link></li>
+              <li className="flex justify-center items-center gap-3"><Link to="/staff/salary" className="hover:text-amber-200 text-xl">Salary</Link></li>
             </>
           )}
 
           {role === "user" && (
             <>
-              <li><Link to="/user/bookings" className="hover:text-amber-200">My Bookings</Link></li>
-              <li><Link to="/user/menu" className="hover:text-amber-200">View Menu</Link></li>
+              <li className="flex justify-center items-center gap-3"><Link to="/user/bookings" className="hover:text-amber-200 text-xl">My Bookings</Link></li>
+              <li className="flex justify-center items-center gap-3"><Link to="/user/menu" className="hover:text-amber-200">View Menu</Link></li>
             </>
           )}
 
-          <li><Link to="/profile" className="hover:text-amber-200">Profile</Link></li>
-          <li><Link to="/logout" className="hover:text-amber-200">Logout</Link></li>
+          <li className="flex justify-center items-center gap-3"><CgProfile className="text-amber-900 text-xl"/><Link to="/profile" className="hover:text-amber-200 text-lg">Profile</Link></li>
+          <li className="flex justify-center items-center gap-3"> <span><BiLogOutCircle className="text-amber-900 text-xl" /></span> <Link to="/logout" className="hover:text-amber-200 text-lg ">Logout</Link></li>
         </ul>
       </div>
     </div>
