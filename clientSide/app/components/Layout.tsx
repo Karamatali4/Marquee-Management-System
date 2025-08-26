6d6a4dc6abaeddc5b9a66d630f56fdd347d282bb
Karamatali4 <maqpoon4@gmail.com>
2025-08-26 14:07:45 +0500

    layout component changes responsive

--- clientSide/app/components/Layout.tsx
+++ clientSide/app/components/Layout.tsx
@@ -1,10 +1,16 @@
+
 import Sidebar from "./Sidebar";
 
 export default function Layout({ children, role }: { children: React.ReactNode; role: string }) {
   return (
-    <div className="flex">
-      <Sidebar role={role} />
-      <main className="flex-1 bg-white p-6">
+    <div className="flex flex-col lg:flex-row min-h-screen">
+      {/* Sidebar */}
+      <div className="w-full lg:w-64 shadow-black">
+        <Sidebar role={role} />
+      </div>
+
+      {/* Main content */}
+      <main className="flex-1 min-h-screen bg-white p-6 ">
         {children}
       </main>
     </div>
